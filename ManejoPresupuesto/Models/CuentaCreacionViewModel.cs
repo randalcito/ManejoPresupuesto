d5759fde87a3d6a6276e3f4088fd6c8c1f89e912
Randal Tristan <randaltristan@gmail.com>
2022-01-28 22:07:33 -0500

    Creacion del tipo de cuenta.

---
+++ ManejoPresupuesto/Models/CuentaCreacionViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace ManejoPresupuesto.Models
+{
+    public class CuentaCreacionViewModel: Cuenta
+    {
+        public IEnumerable<SelectListItem> TiposCuentas { get; set; }
+    }
+}
